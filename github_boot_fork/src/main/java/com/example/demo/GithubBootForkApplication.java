b2dea3d588e7a6712117b6472740852a6751a2b2
CHAYOUBIN <135004128+CHAYOUBIN@users.noreply.github.com>
2023-06-19 10:54:52 +0900

    Update GithubBootForkApplication.java

--- github_boot_fork/src/main/java/com/example/demo/GithubBootForkApplication.java
+++ github_boot_fork/src/main/java/com/example/demo/GithubBootForkApplication.java
@@ -8,7 +8,6 @@ public class GithubBootForkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootForkApplication.class, args);
-		System.out.println("다같이 같은조 기원");
 	}
 
 }
